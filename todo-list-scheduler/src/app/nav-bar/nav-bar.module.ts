e0436d37b17e7c3b520b1010cb78d02a4981ced6
egor.sasim <egor.sasim@altezza.org>
2023-03-11 12:54:12 +0300

    set up nav-bar

--- todo-list-scheduler/src/app/nav-bar/nav-bar.module.ts
+++ todo-list-scheduler/src/app/nav-bar/nav-bar.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NavBarComponent } from 'src/app/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [NavBarComponent],
+  imports: [RouterModule],
   exports: [NavBarComponent],
 })
 export class NavBarModule {}
