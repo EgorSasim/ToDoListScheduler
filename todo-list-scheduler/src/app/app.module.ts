d57de8dda53926fee56f06e868474e611a22d2a4
egor.sasim <egor.sasim@altezza.org>
2023-03-31 13:54:27 +0300

    remove unused import

--- todo-list-scheduler/src/app/app.module.ts
+++ todo-list-scheduler/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { TaskDetailedPageModule } from 'src/app/common/tasks-detailed-page/task-detailed-page.module';
 import { TasksPageModule } from 'src/app/tasks/tasks-page/tasks-page.module';
 import { WithNavBarModule } from 'src/app/with-nav-bar/with-nav-bar.module';
 import { WithoutNavBarModule } from 'src/app/without-nav-bar/without-nav-bar.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
