82cea240f587484746120be04c300cbf15ff4325
egor.sasim <egor.sasim@altezza.org>
2023-05-06 23:05:28 +0300

    add spinner, remove not used staff

--- todo-list-scheduler/src/app/app.module.ts
+++ todo-list-scheduler/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 import { TasksPageModule } from 'src/app/tasks/tasks-page/tasks-page.module';
 import { WithNavBarModule } from 'src/app/with-nav-bar/with-nav-bar.module';
 import { WithoutNavBarModule } from 'src/app/without-nav-bar/without-nav-bar.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AuthorizationPageModule } from 'src/app/authorization-page/authorization-page.module';
+import { SpinnerComponent } from 'src/app/common/spinner/spinner.component';
+import { LoadingInterceptor } from 'src/app/loading.interceptor';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SpinnerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -19,7 +21,13 @@ import { AuthorizationPageModule } from 'src/app/authorization-page/authorizatio
     HttpClientModule,
     AuthorizationPageModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
